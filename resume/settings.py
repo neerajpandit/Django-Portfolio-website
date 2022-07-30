f50677ff2b9e12dc08b4bff40a1c5b3c78b4b922
Neerajpandey1507 <pandeyneeraj1507@gmail.com>
2022-07-30 20:00:08 +0530

    sett

--- resume/settings.py
+++ resume/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['neerajresume.herokuapp.com','127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
